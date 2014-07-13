ed9f58c344f5dbeddf0c564d6ae4af34ef8e59b2
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-07-13 09:50:27 +0430

    Remove Comment.

--- app/SaleBoss/Repositories/AttributeRepositoryInterface.php
+++ app/SaleBoss/Repositories/AttributeRepositoryInterface.php
@@ -1,24 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bigsinoos
- * Date: 07/07/2014
- * Time: 03:35 PM
- */
-
 namespace SaleBoss\Repositories;
 
 use SaleBoss\Models\EntityType;
 
-interface AttributeRepositoryInterface {
+interface AttributeRepositoryInterface
+{
 
-	/**
-	 * Get all attributes of a type
-	 *
-	 * @param EntityType $type
-	 * @return mixed
-	 */
-	public function getAttributesOf(EntityType $type);
+    /**
+     * Get all attributes of a type
+     *
+     * @param EntityType $type            
+     * @return mixed
+     */
+    public function getAttributesOf(EntityType $type);
 
-	public function addAttributes(EntityType $type, $data);
+    public function addAttributes(EntityType $type, $data);
 }
