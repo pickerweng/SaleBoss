d309a67aa9e887b5a8b2cb8fb1fa72647eb37c69
Bigsinoos <pcfeeler@gmail.com>
2014-07-27 14:12:10 +0430

    Remove remove button from group page

--- app/views/admin/pages/group/_operation.blade.php
+++ app/views/admin/pages/group/_operation.blade.php
@@ -1,10 +1,10 @@
 <a href="{{URL::to('groups/' . $group->id . '/edit')}}" class="btn operation-margin btn-xs pull-left btn-info">ویرایش</a>
-<button
+<!--button
 	type="button"
 	class="btn btn-xs pull-left margin-right btn-danger"
 	delete-url="{{URL::to('groups/' . $group->id)}}"
 	onclick="Common.setDeleteURL(this,'#delete_form')"
 	data-toggle="modal"
 	data-target="#removeModal">حذف
-</button>
+</button-->
 
