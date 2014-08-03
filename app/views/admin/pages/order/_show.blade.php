c01c0a86611899fcb63363df63f2cb7c3958dcd5
Bigsinoos <pcfeeler@gmail.com>
2014-08-03 14:19:25 +0430

    Print fix

--- app/views/admin/pages/order/_show.blade.php
+++ app/views/admin/pages/order/_show.blade.php
@@ -13,13 +13,13 @@
             <label class="label label-xs label-info pull-left">{{$order->state->title}}</label>
         </legend>
         <div class="row">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('panel_type', 'نوع پنل ',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$opiloConfig['panel_types'][$order->panel_type]}}</p>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('private_number', 'شماره خط اختصاصی',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->private_number}}</p>
@@ -27,7 +27,7 @@
             </div>
         </div>
 	    <div class="row">
-		    <div class="col-md-6 col-sm-12">
+		    <div class="col-md-6 col-sm-12 col-xs-6">
 			    {{Form::label('customer', 'نام مشتری',['class' => 'control-label'])}}
 			    <p class="form-static-control">{{$order->customer->getIdentifier()}} ({{$order->customer->id}})</p>
 		    </div>
@@ -37,13 +37,13 @@
 		    </div>
 	    </div>
         <div class="row">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('sms_price', 'قیمت هر پیامک به تومان',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->sms_price}}</p>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('sms_quantity', 'تعداد پیامک',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->sms_quantity}}</p>
@@ -51,13 +51,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('sms_text', 'متن پیامک',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->sms_text}}</p>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('sms_description', 'توضیحات پیامک',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->sms_description}}</p>
@@ -65,13 +65,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('payment_type', 'نوع پرداخت',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$opiloConfig['payment_types'][$order->payment_type]}}</p>
                 </div>
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('cart_number', 'شماره کارت',['class' => 'control-label'])}}
                     <p class="form-static-control">{{$order->cart_number}}</p>
@@ -79,11 +79,11 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <label class="control-label">مرحله سفارش</label>
                 <p class="form-control-static">{{$order->state->title}}</p>
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('panel_price', 'مبلغ پنل به تومان',['class' => 'control-label'])}}
                     <p class="form-control-static">{{$order->panel_price}}</p>
@@ -91,7 +91,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12 col-sm-12">
+            <div class="col-md-12 col-sm-12 col-xs-6">
                 <div class="form-group">
                     <br>
                     {{Form::label('last_edit', 'آخرین ویرایش',['class' => 'control-label'])}}
@@ -102,7 +102,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12 col-sm-12">
+            <div class="col-md-12 col-sm-12 col-xs-6">
                 <div class="form-group">
                     {{Form::label('description', 'توضیحات',['class' => 'control-label'])}}
                     <p class="form-control-static">{{$order->description}}</p>
