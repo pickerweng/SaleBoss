20e1fb6c2ebbecef7c03c3ac77ce908bd8b8a6d5
Bigsinoos <pcfeeler@gmail.com>
2014-08-10 11:04:59 +0430

    Minify js and css

--- app/views/admin/blocks/scripts.blade.php
+++ app/views/admin/blocks/scripts.blade.php
@@ -1,6 +1,1 @@
-<script src="{{asset('assets/admin/js/jquery-1.10.2.js')}}"></script>
-<script src="{{asset('assets/admin/js/bootstrap.js')}}"></script>
-<script src="{{asset('assets/admin/js/raphael-min.js')}}"></script>
-<script src="{{asset('assets/admin/js/morris/morris.min.js')}}"></script>
-<script src="{{asset('assets/admin/js/morris/chart-data-morris.js')}}"></script>
-<script src="{{asset('assets/publicJS/jquery.namespace.js')}}"></script>
+<script src="{{asset('assets/admin/js/saleboss.min.js')}}"></script>
--- app/views/admin/blocks/stylesheets.blade.php
+++ app/views/admin/blocks/stylesheets.blade.php
@@ -1,5 +1,2 @@
-<link href="{{asset('assets/admin/css/bootstrap.css')}}" rel="stylesheet">
-<link href="{{asset('assets/admin/css/sb-admin.css')}}" rel="stylesheet">
+<link href="{{asset('assets/admin/css/saleboss.min.css')}}" rel="stylesheet">
 <link rel="stylesheet" href="{{asset('assets/admin/font-awesome/css/font-awesome.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/admin/css/morris.css')}}">
-<link href="{{asset('assets/admin/css/bootstrap-rtl.min.css')}}" rel="stylesheet">
