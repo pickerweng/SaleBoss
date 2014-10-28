776ba6b48b1dbcc0a84e70d2b2aa91a635e7ed2d
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-10-28 16:59:57 +0330

    Change Local Database name

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'saleboss',
+			'database'  => 'sale_boss',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
