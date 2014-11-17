7ce1fe3dfb9878044de17cae8391b27266637e2d
Saeed Habibi <saeeditce.habibi@gmail.com>
2014-11-17 13:18:47 +0330

    Fix datTime for leads period

--- app/Controllers/LeadController.php
+++ app/Controllers/LeadController.php
@@ -234,16 +234,18 @@ class LeadController extends BaseController
         $firstTime = Input::get('from');
         $secondTime = Input::get('to');
 
+
+
         if (!empty($firstTime) && !empty($secondTime)) {
             $firstTimeEx = explode('-', $firstTime);
             $firstTimeG = Jalali\jDateTime::toGregorian($firstTimeEx[0], $firstTimeEx[1], $firstTimeEx[2]);
-            $firstTime = $firstTimeG[0] . '-' . $firstTimeG[1] . '-' . $firstTimeG[2] . ' 00:00';
+            $firstTime = $firstTimeG[0] . '-' . $firstTimeG[1] . '-' . $firstTimeG[2] . ' 00:00:00 00:00';
 
             $secondTimeEx = explode('-', $secondTime);
             $secondTimeG = Jalali\jDateTime::toGregorian($secondTimeEx[0], $secondTimeEx[1], $secondTimeEx[2]);
-            $secondTime = $secondTimeG[0] . '-' . $secondTimeG[1] . '-' . $secondTimeG[2] . ' 00:00';
+            $secondTime = $secondTimeG[0] . '-' . $secondTimeG[1] . '-' . $secondTimeG[2] . ' 00:00:00 00:00';
         }
-
+        
         $leads = $this->execute(ListWithTimeCommand::class, compact('user','firstTime', 'secondTime'));
         return $this->view('admin.pages.lead.user_between', compact('user', 'leads'));
     }
